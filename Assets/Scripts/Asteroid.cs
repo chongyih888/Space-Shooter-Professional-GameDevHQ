7e91420090e5b63888cdea077f27fd2c4805b0cd
Lim Chong Yih <chongyih21@gmail.com>
2019-12-11 09:17:01 +0800

    initial commit

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    [SerializeField]
+    private float _rotateSpeed = 3.0f;
+
+    [SerializeField]
+    private GameObject _explosionPrefab;
+
+    private SpawnManager _spawnManager;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _spawnManager = GameObject.Find("Spawn_Manager").GetComponent<SpawnManager>();
+
+        if(_spawnManager == null)
+        {
+            Debug.LogError("The SpawnManager is NULL");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //rotate object on the z axis
+        transform.Rotate(Vector3.forward * _rotateSpeed * Time.deltaTime);
+
+    }
+
+    //check for Laser collision(Trigger)
+    //instantiate explosion at the position of the asteriod (us)
+    //destroy the explosion after 3 seconds
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Laser")
+        {
+            Instantiate(_explosionPrefab, transform.position, Quaternion.identity);
+            Destroy(other.gameObject);
+            Destroy(GetComponent<Collider2D>());
+            _spawnManager.StartSpawning();
+            Destroy(this.gameObject,0.5f);
+        }
+    }
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField]
+    private Text _scoreText;
+
+    [SerializeField]
+    private Image _LivesImg;
+
+    [SerializeField]
+    private Sprite[] _liveSprites;
+
+    [SerializeField]
+    private Text _gameOverText;
+
+    [SerializeField]
+    private Text _restartText;
+
+    private GameManager _gameManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {        
+        //assign text component to the handle
+        _scoreText.text = "Score: " + 0;
+        _gameManager = GameObject.Find("Game_Manager").GetComponent<GameManager>();
+
+        if(_gameManager == null)
+        {
+            Debug.LogError("The GameMamanger is NULL");
+        }
+    }
+
+    public void UpdateScore(int playerScore)
+    {
+        _scoreText.text = "Score: " + playerScore;
+    }
+
+    public void UpdateLives(int currentLives)
+    {
+        //display img sprite
+        //give it a new one based on the currentLives index
+        if (currentLives > -1 && currentLives < 4)
+        {
+            _LivesImg.sprite = _liveSprites[currentLives];
+        }
+
+        if(currentLives < 1)
+        {
+            GameOverSequence();
+        }
+    }
+
+    void GameOverSequence()
+    {
+        _gameManager.GameOver();
+        _gameOverText.gameObject.SetActive(true);
+        _restartText.gameObject.SetActive(true);
+        StartCoroutine(GameOverFlickerRoutine());
+
+    }
+
+    IEnumerator GameOverFlickerRoutine()
+    {
+        while (true)
+        {
+            _gameOverText.text = "GAME OVER";
+            yield return new WaitForSeconds(0.5f);
+            _gameOverText.text = "";
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+}
